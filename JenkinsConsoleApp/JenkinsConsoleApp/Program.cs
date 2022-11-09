3b41c19e885922d196389755bf9825b96325da2a
Reema Hamed <rhamed1986@gmail.com>
2022-11-09 14:44:54 +0200

    Adding new console App

---
+++ JenkinsConsoleApp/JenkinsConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! this is my first app on GitHub");
+        }
+    }
+}
